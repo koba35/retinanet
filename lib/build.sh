130d6bc57a940679980dad0632b54178816e3855
koba35 <kobaqhat@gmail.com>
2018-09-09 22:25:49 +0300

    Update build.sh
    update

--- lib/build.sh
+++ lib/build.sh
@@ -9,7 +9,7 @@ CUDA_ARCH="-gencode arch=compute_30,code=sm_30 \
 # Build NMS
 cd nms/src/cuda
 echo "Compiling nms kernels by nvcc..."
-/usr/local/cuda/bin/nvcc -c -o nms_kernel.cu.o nms_kernel.cu -x cu -Xcompiler -fPIC $CUDA_ARCH
+/usr/bin/nvcc -c -o nms_kernel.cu.o nms_kernel.cu -x cu -Xcompiler -fPIC $CUDA_ARCH
 cd ../../
 python build.py
 cd ../
